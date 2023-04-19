9fe07c332e5be56993f4fbdbc6b1d77815a26736
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-04-19 15:27:33 +0000

    removed state storage

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist, createJSONStorage, StateStorage } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export interface User {
