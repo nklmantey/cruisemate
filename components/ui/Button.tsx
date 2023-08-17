3d2865e222590dd4c0cfb9948bbcfa2ec8f93756
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-08-17 13:53:52 +0000

    changes to button component to render icons with button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -7,12 +7,16 @@ import {
 import { BoldText } from "../StyledText";
 import Colors from "../../constants/Colors";
 import useColorScheme from "../../hooks/useColorScheme";
+import { View } from "../Themed";
 
 type ButtonProps = {
   title: string | JSX.Element;
   style?: ViewStyle;
   textStyle?: TextStyle;
   onPress: () => void;
+  disabled?: boolean;
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
 };
 
 export function PrimaryButton({
@@ -20,28 +24,81 @@ export function PrimaryButton({
   style,
   textStyle,
   onPress,
+  disabled,
+  leftIcon,
+  rightIcon,
 }: ButtonProps) {
   const theme = useColorScheme();
 
   return (
     <TouchableOpacity
+      disabled={disabled}
       onPress={onPress}
       style={[
         styles.primaryButton,
-        { backgroundColor: Colors[theme].text, ...style },
+        { backgroundColor: disabled ? "gray" : Colors[theme].text, ...style },
       ]}
     >
-      <BoldText
-        style={[
-          {
-            color: Colors[theme].background,
-            fontSize: 17,
-            ...textStyle,
-          },
-        ]}
-      >
-        {title}
-      </BoldText>
+      {leftIcon && (
+        <View
+          style={{
+            backgroundColor: "transparent",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          {leftIcon}
+          <BoldText
+            style={[
+              {
+                color: Colors[theme].background,
+                fontSize: 17,
+                ...textStyle,
+              },
+            ]}
+          >
+            {title}
+          </BoldText>
+        </View>
+      )}
+
+      {rightIcon && (
+        <View
+          style={{
+            backgroundColor: "transparent",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <BoldText
+            style={[
+              {
+                color: Colors[theme].background,
+                fontSize: 17,
+                ...textStyle,
+              },
+            ]}
+          >
+            {title}
+          </BoldText>
+          {rightIcon}
+        </View>
+      )}
+
+      {!leftIcon && !rightIcon && (
+        <BoldText
+          style={[
+            {
+              color: Colors[theme].background,
+              fontSize: 17,
+              ...textStyle,
+            },
+          ]}
+        >
+          {title}
+        </BoldText>
+      )}
     </TouchableOpacity>
   );
 }
@@ -74,10 +131,10 @@ export function SecondaryButton(props: any) {
 const styles = StyleSheet.create({
   primaryButton: {
     width: "100%",
-    borderRadius: 8,
+    borderRadius: 100,
     alignSelf: "center",
     backgroundColor: "transparent",
-    height: 52,
+    height: 44,
     alignItems: "center",
     justifyContent: "center",
     marginVertical: 8,
