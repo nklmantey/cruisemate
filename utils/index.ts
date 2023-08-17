cb89dc5a38bc41f066f33aa3a86ce1ad7254ca92
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-08-17 13:53:17 +0000

    validation utils

--- utils/index.ts
+++ utils/index.ts
@@ -15,4 +15,26 @@ function money(n: any, d = 2) {
   return d === 2 ? numeral(n).format("0,0.00") : numeral(n).format("0,0.000");
 }
 
-export { greetings, money };
+function validateEmail(email: string): boolean {
+  const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+$/;
+  return regex.test(email) && email.trim().length > 0;
+}
+
+function validatePassword(password: string): boolean {
+  return password.length >= 8 && password.trim().length > 0;
+}
+
+function validateMatchPassword(
+  password: string,
+  confirmPassword: string
+): boolean {
+  return password.trim() === confirmPassword.trim();
+}
+
+export {
+  greetings,
+  money,
+  validateEmail,
+  validatePassword,
+  validateMatchPassword,
+};
